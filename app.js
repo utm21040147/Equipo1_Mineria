205bf5ff3369d321505af9d77d469ab19489921a
Isaac Reyes - UTM21040147 <utm21040147@utma.edu.mx>
2025-06-20 16:33:55 -0600

    EQUIPO 1 ALUMNOS

---
+++ app.js
@@ -0,0 +1,28 @@
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+import express from "express";
+import cors from "cors";
+import { test } from 
+
+
+dotenv.config();
+//Conexion a Mongoose
+mongoose.connect(process.env.urlbd)
+//Si conecta manda este mensaje
+.then(() => {
+    console.log("Connected to MongoDB");
+} )
+//Si no conecta manda este mensaje
+.catch((error) => {
+    console.error("Error connecting to MongoDB:", error);
+});
+
+const app = express();
+//Seguridad
+app.use(cors()); 
+app.listen(4000, () => {
+    console.log("ESCUCHANDO AL SERVIDOR EN EL PUERTO 4000");
+});
+
+
+test(); 
